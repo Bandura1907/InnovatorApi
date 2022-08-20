cadd32544702752cd96d891a4dc9f89d26143524
tarasbandura <taras190789@gmail.com>
2022-08-20 19:00:34 +0300

    useful finish

--- src/main/java/com/innovator/innovator/InnovatorApplication.java
+++ src/main/java/com/innovator/innovator/InnovatorApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,6 +21,7 @@ import java.util.Collections;
 
 @SpringBootApplication
 @EnableAsync
+@EnableCaching
 public class InnovatorApplication {
 
     public static void main(String[] args) {
