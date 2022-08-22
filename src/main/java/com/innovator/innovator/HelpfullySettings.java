2a4c57fe37832c27881c4c1ae3a52c4f8d7814bf
tarasbandura <taras190789@gmail.com>
2022-08-22 15:03:18 +0300

    useful finish

--- src/main/java/com/innovator/innovator/HelpfullySettings.java
+++
@@ -1,8 +0,0 @@
-package com.innovator.innovator;
-
-import java.time.ZoneId;
-import java.util.TimeZone;
-
-public class HelpfullySettings {
-    static public ZoneId ZONE_DEFAULT = TimeZone.getTimeZone("Europe/Moscow").toZoneId();
-}
